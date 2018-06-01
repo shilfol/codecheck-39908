9ac2680c991d52f4aae375f21f618289f433cbe0
shilfol <yayananoda@gmail.com>
2018-06-01 13:29:37 +0900

    solve

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 )
 
 func main() {
-  // このコードは引数と標準出力を用いたサンプルコードです。
-  // このコードは好きなように編集・削除してもらって構いません。
-  // ---
-  // This is a sample code to use arguments and outputs.
-  // Edit and remove this code as you like.
+	// このコードは引数と標準出力を用いたサンプルコードです。
+	// このコードは好きなように編集・削除してもらって構いません。
+	// ---
+	// This is a sample code to use arguments and outputs.
+	// Edit and remove this code as you like.
 
 	run(os.Args[1:])
 }
 
 func run(args []string) {
-	for i, v := range args {
-		fmt.Printf("argv[%d]: %s\n", i, v)
-	}
+	fmt.Print("Hello ", args[0], "!")
+	fmt.Println()
+
 }
